ad813d027921cc1701168cc3bcff42664b2974d3
Joshua Pangborn <joshua.pangborn@gmail.com>
2016-02-27 23:45:28 -0500

    Initial Commit

---
+++ sitemap.php
@@ -0,0 +1,20 @@
+<?php
+
+  $excludepages = c::get('sitemap.exclude.pages', array('error'));
+  $excludetemplates = c::get('sitemap.exclude.templates', array());
+  $important = c::get('sitemap.important', array());
+
+  kirby()->routes(array(
+    array(
+      'pattern' => 'sitemap.xml',
+      'action' => function() use ($excludepages, $excludetemplates, $important) {
+        $sitemap = tpl::load(__DIR__ . DS . 'template.php', array(
+          'pages' => site()->pages(),
+          'excludepages' => $excludepages,
+          'excludetemplates' => $excludetemplates,
+          'important' => $important), true);
+
+        return new Response($sitemap, 'xml');
+      }
+    )
+  ));
---
+++ template.php
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <?php foreach($pages->index() as $p): ?>
+  <?php if(in_array($p->uri(), $excludepages)) continue ?>
+  <?php if(in_array($p->template(), $excludetemplates)) continue ?>
+  <url>
+    <loc><?= html($p->url()) ?></loc>
+    <lastmod><?= $p->modified('c') ?></lastmod>
+    <priority><?= ($p->isHomePage() || in_array($p->uri(), $important)) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
+  </url>
+  <?php endforeach ?>
+</urlset>
